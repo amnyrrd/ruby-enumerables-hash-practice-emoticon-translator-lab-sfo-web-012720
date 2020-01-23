42d8d2249cd98a358e85536561280a3084e08d79
Aaron Minyard <amnyrrd@gmail.com>
2020-01-23 23:34:10 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,12 +16,12 @@ end
 
 def get_japanese_emoticon(yaml_path, emoticon)
   lib = load_library('./lib/emoticons.yml')
-  emo = emoticons[:get_emoticon][emoticon]
-  if emo
-    puts emo
-  else
-    puts "Sorry!"
-  end
+  # emo = emoticons[:get_emoticon][emoticon]
+  # if emo
+  #   puts emo
+  # else
+  #   puts "Sorry!"
+  # end
 end
 
 def get_english_meaning
