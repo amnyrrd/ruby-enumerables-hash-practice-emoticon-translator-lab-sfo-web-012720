223f7629bbed820d1e73e1e2b01f61b5ef92e40f
Aaron Minyard <amnyrrd@gmail.com>
2020-01-23 23:09:07 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,7 @@ require 'pp'
 def load_library(lib)
   emoticon_hash = YAML.load_file('./lib/emoticons.yml')
   new_hash = { "get_meaning": {}, "get_emoticon": {} } 
-  new_hash[:get_meaning] = emoticon_hash.keys.to_s
+  new_hash[:get_meaning] = emoticon_hash.keys
 end
 
 def get_japanese_emoticon
